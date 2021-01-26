10fe4050834e60304a91e819a32fc299d430d718
Jason B <jboring555@gmail.com>
2021-01-25 22:57:58 -0500

    up

--- control_scripts/update_all_lab.cmd
+++ control_scripts/update_all_lab.cmd
@@ -1,6 +1,6 @@
 ansible-playbook -i /home/mercadmin/ansible/windows_lab_v1/inventory /home/mercadmin/ansible/windows_lab_v1/labplaybooks/win_core_update.yml -vv
 ansible-playbook -i /home/mercadmin/ansible/windows_lab_v1/inventory /home/mercadmin/ansible/windows_lab_v1/labplaybooks/win_sec_update.yml -vv
 ansible-playbook -i /home/mercadmin/ansible/windows_lab_v1/inventory /home/mercadmin/ansible/windows_lab_v1/labplaybooks/win_app_update.yml -vv
-ansible-playbook -i /home/mercadmin/ansible/centos_lab_v1/inventory /home/mercadmin/ansible/centos_lab_v1/labplaybooks/centos_knl_upgrade.yml -vv
-ansible-playbook -i /home/mercadmin/ansible/centos_lab_v1/inventory /home/mercadmin/ansible/centos_lab_v1/labplaybookscentos_pkg_upgrade.yml -vv
-ansible-playbook -i /home/mercadmin/ansible/centos_lab_v1/inventory /home/mercadmin/ansible/ubtuntu_lab_v2/labplaybooks/ubuntu_upgrade.yml -vv
+ansible-playbook -i /home/mercadmin/ansible/centos_lab_v2/inventory /home/mercadmin/ansible/centos_lab_v2/labplaybooks/centos_knl_upgrade.yml -vv
+ansible-playbook -i /home/mercadmin/ansible/centos_lab_v2/inventory /home/mercadmin/ansible/centos_lab_v2/labplaybooks/centos_pkg_upgrade.yml -vv
+ansible-playbook -i /home/mercadmin/ansible/ubtuntu_lab_v3/inventory /home/mercadmin/ansible/ubtuntu_lab_v3/labplaybooks/ubuntu_upgrade.yml -vv
